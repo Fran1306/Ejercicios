1ec6b92d22a411ad3fa3215f290c069f89163c24
asixdam1b <Fran4494@gmail.com>
2018-11-06 16:45:00 +0100

    Ejercicio 324

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,29 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        int uvas = 100;
+
+        while(uvas !=0) {
+            uvas = teclado.nextInt();
+
+            int niveles = 0;
+            int total = 0;
+
+            while (uvas > total) {
+                niveles++;
+                total = niveles + total;
+            }
+
+            if(niveles != 0) {
+                System.out.println(niveles);
+            }
+        }
 
     }
 }
